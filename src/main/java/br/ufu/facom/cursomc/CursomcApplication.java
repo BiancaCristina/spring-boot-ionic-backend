43efc5d0b1ec652b145b51e101f161116734f456
Bianca Cristina <phon-bia@hotmail.com>
2018-07-21 12:52:18 -0300

    Alterando porta padrao

--- src/main/java/br/ufu/facom/cursomc/CursomcApplication.java
+++ src/main/java/br/ufu/facom/cursomc/CursomcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication {
-
+	// Para alterar porta, vai no resources e coloca: server.port=${port:8081}, 8081 eh a porta nova!
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
