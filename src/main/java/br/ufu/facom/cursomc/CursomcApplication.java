88d8bece2c669c80eb6fe9b3f8029358b75bc34e
BiancaCristina <phon-bia@hotmail.com>
2018-07-21 21:47:59 -0300

    Criando operacao de instanciacao

--- src/main/java/br/ufu/facom/cursomc/CursomcApplication.java
+++ src/main/java/br/ufu/facom/cursomc/CursomcApplication.java
@@ -1,12 +1,35 @@
 package br.ufu.facom.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.ufu.facom.cursomc.domain.Categoria;
+import br.ufu.facom.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 	// Para alterar porta, vai no resources e coloca: server.port=${port:8081}, 8081 eh a porta nova!
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository; // Repositorio eh quem salva dados
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		// Array as List = cria Lista automatica e posso colocar quantos elementos eu quiser dentro
+		
+	}
+	
+	
 }
