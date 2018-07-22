e886cff05f24bd3e7a1de1b4e2c847432e1d45d3
BiancaCristina <phon-bia@hotmail.com>
2018-07-22 13:41:24 -0300

    Criacao das classes Cidade e Estado

--- src/main/java/br/ufu/facom/cursomc/CursomcApplication.java
+++ src/main/java/br/ufu/facom/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.ufu.facom.cursomc.domain.Categoria;
+import br.ufu.facom.cursomc.domain.Cidade;
+import br.ufu.facom.cursomc.domain.Estado;
 import br.ufu.facom.cursomc.domain.Produto;
 import br.ufu.facom.cursomc.repositories.CategoriaRepository;
+import br.ufu.facom.cursomc.repositories.CidadeRepository;
+import br.ufu.facom.cursomc.repositories.EstadoRepository;
 import br.ufu.facom.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository; 
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -37,6 +47,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Produto p2 = new Produto(null,"Impressora",800.00);
 		Produto p3 = new Produto(null,"Mouse",80.00);
 		
+		// Instancia dos estados
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"Sao Paulo");
+		
+		// Instancia das cidades
+		Cidade c1 = new Cidade(null,"Uberlandia", est1);
+		Cidade c2 = new Cidade(null,"Sao Paulo",est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		
+		// Adicionando cidades nos estados
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
 		// Adicionando os produtos em cada categoria
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -49,6 +72,8 @@ public class CursomcApplication implements CommandLineRunner{
 		// Salvando no repositorio
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		// Array as List = cria Lista automatica e posso colocar quantos elementos eu quiser dentro
 		
 	}
