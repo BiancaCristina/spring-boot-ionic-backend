429d83f77ade44d7bd57da9ba9aa5d5fa49b7a14
BiancaCristina <phon-bia@hotmail.com>
2018-07-23 15:57:29 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/br/ufu/facom/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/ufu/facom/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,10 +28,11 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}",method= RequestMethod.GET) // REQUISICAO BASICA = GET
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-		// @PathVariable = faz com que {id} corresponda ao id passado como parametro
+		// @PathVariable = faz com que {id} corresponda ao id passado como parametro (caminho da URL)
 		// ResponseEntity = armazena varias info de uma resposta HTTP para um servico REST			
 		
 		Categoria obj = service.find(id);
+		
 		return ResponseEntity.ok().body(obj);
 	}
 	
@@ -56,18 +59,39 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}",method= RequestMethod.DELETE) // REQUISICAO BASICA = GET
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-		service.delete(id);
+		service.delete(id);		
 		
 		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping(method= RequestMethod.GET) // REQUISICAO BASICA = GET
-	public ResponseEntity<List<CategoriaDTO>> findAll() {
-		
+	public ResponseEntity<List<CategoriaDTO>> findAll() {	
 		List <Categoria> list = service.findAll();
+		
 		// O codigo abaixo converte cada objeto da lista de Categoria em um objeto de CategoriaDTO e forma uma lista de CategoriaDTO
 		// CategoriaDTO mostrara apenas os dados que me interessam quando estou imprimindo a lista de Categorias(id,nome)
 		List <CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/page",method= RequestMethod.GET) // REQUISICAO BASICA = GET
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(name="page",defaultValue="0") Integer page, 
+			@RequestParam(name="linesPerPage",defaultValue="24") Integer linesPerPage, 
+			@RequestParam(name="orderBy",defaultValue="nome") String orderBy, 
+			@RequestParam(name="direction",defaultValue="ASC") String direction) // FIM DOS PARAMETROS DO METODO
+	{	
+		// defaultValue = valor padrao -> se nao for informado nenhum valor, assume o valor padrao
+		// defaultValue do direction pode ser ASC(ascendente) ou DESC(descendente)
+		// defaultValue do orderBy = por qual parametro devo ordenar as paginas
+		// defaultValue do linesPerPage = numero de Categorias informadas por pagina 
+		// Essa funcao faz a paginacao (divisao de Categoria por pagina)
+		
+		Page<Categoria> list = service.findPage(page,linesPerPage,orderBy,direction);
+		
+		// O codigo abaixo converte cada objeto da lista de Categoria em um objeto de CategoriaDTO e forma uma lista de CategoriaDTO
+		// CategoriaDTO mostrara apenas os dados que me interessam quando estou imprimindo a lista de Categorias(id,nome)
+		Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj)); // Retirei "stream" e "collect"
 		return ResponseEntity.ok().body(listDTO);
 	}
 }
