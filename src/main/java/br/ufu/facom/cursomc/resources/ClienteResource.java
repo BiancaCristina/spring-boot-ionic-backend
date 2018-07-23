cb8297f9122a78941145aee6e433878962030cb3
BiancaCristina <phon-bia@hotmail.com>
2018-07-23 18:47:57 -0300

    Criacao ClienteDTO e atualizacao do ClienteResource e ClienteService (possui alguns erros no POST porque falta certas implementacoes)

--- src/main/java/br/ufu/facom/cursomc/resources/ClienteResource.java
+++ src/main/java/br/ufu/facom/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package br.ufu.facom.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufu.facom.cursomc.domain.Cliente;
+import br.ufu.facom.cursomc.dto.ClienteDTO;
 import br.ufu.facom.cursomc.services.ClienteService;
 
 @RestController
@@ -24,5 +33,53 @@ public class ClienteResource {
 			
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody @Valid ClienteDTO objDTO, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDTO);
+		
+		obj.setId(id); // Comando feito pra garantir que o objeto que eu to passando tenha ID que eu quero atualizar
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}",method= RequestMethod.DELETE) // REQUISICAO BASICA = GET
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);		
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method= RequestMethod.GET) // REQUISICAO BASICA = GET
+	public ResponseEntity<List<ClienteDTO>> findAll() {	
+		List <Cliente> list = service.findAll();
+		
+		// O codigo abaixo converte cada objeto da lista de Cliente em um objeto de ClienteDTO e forma uma lista de ClienteDTO
+		// ClienteDTO mostrara apenas os dados que me interessam quando estou imprimindo a lista de Clientes(id,nome)
+		List <ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/page",method= RequestMethod.GET) // REQUISICAO BASICA = GET
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(name="page",defaultValue="0") Integer page, 
+			@RequestParam(name="linesPerPage",defaultValue="24") Integer linesPerPage, 
+			@RequestParam(name="orderBy",defaultValue="nome") String orderBy, 
+			@RequestParam(name="direction",defaultValue="ASC") String direction) // FIM DOS PARAMETROS DO METODO
+	{	
+		// defaultValue = valor padrao -> se nao for informado nenhum valor, assume o valor padrao
+		// defaultValue do direction pode ser ASC(ascendente) ou DESC(descendente)
+		// defaultValue do orderBy = por qual parametro devo ordenar as paginas
+		// defaultValue do linesPerPage = numero de Clientes informadas por pagina 
+		// Essa funcao faz a paginacao (divisao de Cliente por pagina)
+		
+		Page<Cliente> list = service.findPage(page,linesPerPage,orderBy,direction);
+		
+		// O codigo abaixo converte cada objeto da lista de Cliente em um objeto de ClienteDTO e forma uma lista de ClienteDTO
+		// ClienteDTO mostrara apenas os dados que me interessam quando estou imprimindo a lista de Clientes(id,nome)
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj)); // Retirei "stream" e "collect"
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/br/ufu/facom/cursomc/services/ClienteService.java
+++ src/main/java/br/ufu/facom/cursomc/services/ClienteService.java
@@ -1,13 +1,21 @@
 package br.ufu.facom.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.ufu.facom.cursomc.domain.Cliente;
+import br.ufu.facom.cursomc.domain.Cliente;
+import br.ufu.facom.cursomc.dto.ClienteDTO;
 import br.ufu.facom.cursomc.repositories.ClienteRepository;
-import br.ufu.facom.cursomc.services.exceptions.*;
+import br.ufu.facom.cursomc.services.exceptions.DataIntegrityException;
+import br.ufu.facom.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
@@ -20,5 +28,47 @@ public class ClienteService {
 		// TRATAMENTO DE ERRO PARA CASO NAO EXISTA OBJETO
 		return obj.orElseThrow( () -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update (Cliente obj) {
+		// A diferenca entre update e inser eh que em um eu preciso garantir que ID seja nulo (insert),
+		// No outro, caso o ID nao seja nulo, entao irei fazer uma atualizacao em um ID que ja existe no meu BD
+		
+		// Verifica se o objeto existe
+		this.find(obj.getId()); // Caso o objeto nao exista, esse metodo ja lanca a excecao!
+		
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		// Verifica se ID existe
+		this.find(id);
+		
+		try 
+		{			
+			repo.deleteById(id);
+		}
+		
+		catch (DataIntegrityViolationException e)
+		{
+			throw new DataIntegrityException("Nao eh possivel excluir uma categoria que possui produtos");
+		}		
+	}
+	
+	public List<Cliente> findAll(){
+		// Esse metodo retorna todas as categorias quando digitar o URL "/categoria"
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		// Metodo auxiliar que instancia um objeto do tipo Cliente a partir de um objeto do tipo ClienteDTO
+		//return new Cliente(objDTO.getId(),objDTO.getNome());
+		throw new UnsupportedOperationException(); // Solucao temporaria, tenho que implementar aqui depois
 	}
 }
