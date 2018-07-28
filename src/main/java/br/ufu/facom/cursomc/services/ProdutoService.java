5fe6778f072b388386f739bd2fbffa818802d0f2
BiancaCristina <phon-bia@hotmail.com>
2018-07-28 19:40:01 -0300

    Busca de pedidos por nome e categoria - Sem usar @Query, usando busca pre-definida do Spring JPA

--- src/main/java/br/ufu/facom/cursomc/services/ProdutoService.java
+++ src/main/java/br/ufu/facom/cursomc/services/ProdutoService.java
@@ -37,7 +37,7 @@ public class ProdutoService {
 		
 		List<Categoria> categorias = categoriaRepository.findAllById(ids); // Gera a lista de categorias a partir da lista de IDS (ids)
 		
-		return repo.search(nome, categorias, pageRequest);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
 		
 	}
 }
