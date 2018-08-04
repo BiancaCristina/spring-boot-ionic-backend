a72ca4be21916befc657581b0868decf0272d556
BiancaCristina <phon-bia@hotmail.com>
2018-08-04 18:36:31 -0300

    Refresh token

---
+++ src/main/java/br/ufu/facom/cursomc/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package br.ufu.facom.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufu.facom.cursomc.security.JWTUtil;
+import br.ufu.facom.cursomc.security.UserSS;
+import br.ufu.facom.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	// Essa classe eh responsavel por disponibilizar endpoints relacionados a autorizacao e autenticacao
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		// Esse metodo renova o token do usuario LOGADO 
+		UserSS user = UserService.authenticated(); // Pega usuario logado
+		String token = jwtUtil.generateToken(user.getUsername()); // Gera novo token
+		response.addHeader("Authorization", "Bearer " + token); // Adicionad token
+		return ResponseEntity.noContent().build();
+	}
+}
