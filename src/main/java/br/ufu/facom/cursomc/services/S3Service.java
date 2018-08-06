670e0450a65661dec9f0a1b502009fd7e5059e39
BiancaCristina <phon-bia@hotmail.com>
2018-08-06 00:52:16 -0300

    Salvando primeiro arquivo no S3

---
+++ src/main/java/br/ufu/facom/cursomc/services/S3Service.java
@@ -0,0 +1,46 @@
+package br.ufu.facom.cursomc.services;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+	
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+	
+	@Autowired
+	private AmazonS3 s3client;
+	
+	@Value("${s3.bucket}")
+	private String bucketName; // Pega o nome do meu bucket no AmazonS3
+	
+	public void uploadFile(String localFilePath) {
+		// Esse metodo faz o upload de arquivos la pro AmazonS3
+		try {
+		File file = new File(localFilePath);
+		LOG.info("Iniciando upload"); 
+		s3client.putObject(new PutObjectRequest(bucketName, file.getName(), file));
+		LOG.info("Upload finalizado");
+		}
+		
+		catch (AmazonServiceException e) {
+			// Caso aconteca algum erro
+			LOG.info("AmazonServiceException: " + e.getErrorMessage());
+			LOG.info("Status code: " + e.getErrorCode());
+		}
+		
+		catch (AmazonClientException e) {
+			LOG.info("AmazonClientException: " + e.getMessage());
+		}
+	}
+}
